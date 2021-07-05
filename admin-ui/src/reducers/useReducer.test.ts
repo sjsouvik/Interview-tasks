15fa2e7f533e9adec1544d904910a99e5542d75c
Souvik Jana <souvikjana2013@gmail.com>
2021-07-05 22:05:26 +0530

    feat: added tests for reducer function

--- admin-ui/src/reducers/useReducer.test.ts
+++ admin-ui/src/reducers/useReducer.test.ts
@@ -1,6 +1,147 @@
-import { useReducer } from "react";
-import { userReducer } from "./userReducer";
+import { userState, userReducer } from "./userReducer";
 import { Action } from "./userReducer.types";
+
+describe("testing initializing data feature", () => {
+  test("should initialize data when ASSIGN_DATA action is dispatched", () => {
+    let action: Action = {
+      type: "ASSIGN_DATA",
+      payload: {
+        name: "users",
+        data: [
+          {
+            id: 1,
+            name: "Arvind Kumar",
+            email: "arvind@geektrust.com",
+            role: "admin",
+          },
+          {
+            id: 2,
+            name: "Chetan Kumar",
+            email: "chetan@gmail.com",
+            role: "member",
+          },
+          {
+            id: 3,
+            name: "Aishwariya Naik",
+            email: "aishwariya@gmail.com",
+            role: "member",
+          },
+        ],
+      },
+    };
+
+    let updatedState = userReducer(userState, action);
+
+    expect(updatedState).toEqual({
+      users: [
+        {
+          id: 1,
+          name: "Arvind Kumar",
+          email: "arvind@geektrust.com",
+          role: "admin",
+        },
+        {
+          id: 2,
+          name: "Chetan Kumar",
+          email: "chetan@gmail.com",
+          role: "member",
+        },
+        {
+          id: 3,
+          name: "Aishwariya Naik",
+          email: "aishwariya@gmail.com",
+          role: "member",
+        },
+      ],
+      searchText: "",
+      currentPage: 1,
+      loading: false,
+      error: false,
+    });
+  });
+});
+
+describe("testing select or unselect rows feature", () => {
+  test("should select or unselect if selected before when SELECT_OR_UNSELECT_ALL_ROWS action is dispatched", () => {
+    const userState = {
+      users: [
+        {
+          id: 1,
+          name: "Arvind Kumar",
+          email: "arvind@geektrust.com",
+          role: "admin",
+        },
+        {
+          id: 2,
+          name: "Chetan Kumar",
+          email: "chetan@gmail.com",
+          role: "member",
+        },
+        {
+          id: 3,
+          name: "Aishwariya Naik",
+          email: "aishwariya@gmail.com",
+          role: "member",
+        },
+      ],
+      searchText: "",
+      currentPage: 1,
+      loading: false,
+      error: false,
+    };
+
+    let action: Action = {
+      type: "SELECT_OR_UNSELECT_ALL_ROWS",
+      payload: {
+        users: [
+          {
+            id: 2,
+            name: "Chetan Kumar",
+            email: "chetan@gmail.com",
+            role: "member",
+          },
+          {
+            id: 3,
+            name: "Aishwariya Naik",
+            email: "aishwariya@gmail.com",
+            role: "member",
+          },
+        ],
+      },
+    };
+
+    const updatedState = userReducer(userState, action);
+
+    expect(updatedState).toEqual({
+      users: [
+        {
+          id: 1,
+          name: "Arvind Kumar",
+          email: "arvind@geektrust.com",
+          role: "admin",
+        },
+        {
+          id: 2,
+          name: "Chetan Kumar",
+          email: "chetan@gmail.com",
+          role: "member",
+          selected: true,
+        },
+        {
+          id: 3,
+          name: "Aishwariya Naik",
+          email: "aishwariya@gmail.com",
+          role: "member",
+          selected: true,
+        },
+      ],
+      searchText: "",
+      currentPage: 1,
+      loading: false,
+      error: false,
+    });
+  });
+});
 
 describe("testing ready to edit feature", () => {
   test("should make one record enable or disable to edit when READY_TO_EDIT action is dispatched", () => {
@@ -193,7 +334,7 @@ describe("testing delete user feature", () => {
 });
 
 describe("testing delete selected users feature", () => {
-  test("should delete selected users when action is dispatched", () => {
+  test("should delete selected users when DELETE_USERS action is dispatched", () => {
     const userState = {
       users: [
         {
